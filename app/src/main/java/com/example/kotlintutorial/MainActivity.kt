eafb45649b0cc6a75d87da2c9f3acce82ae0a59f
acer <wongsatorn1501003@gmail.com>
2019-10-15 13:54:45 +0700

    17

--- app/src/main/java/com/example/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial/MainActivity.kt
@@ -1,8 +1,8 @@
-fun main(arg:Array<String>){
-    println("Hello world")
+fun main(arg: Array<String>) {
+    /*println("Hello world")
     println("My first Kotlin program")
 
-    val jim  = "jame T kirk"
+    val jim = "jame T kirk"
     val tim = "tim Buchalka"
     println(tim)
 
@@ -14,19 +14,27 @@ fun main(arg:Array<String>){
     println()
 
     val apples = 6
-    val oranges= 5
-    val  frult  = apples - oranges
+    val oranges = 5
+    val frult = apples - oranges
     println("$apples apples - $oranges oranges leaves $frult place(s) of fruit")
 
     println("A quarter of the apples is ${apples / 4}")
 
     println()
     val week: Int = 128
-    val  year: Double = week / 52.0
+    val year: Double = week / 52.0
     println("130 weeks in year is : " + year)
 
     //tim = "jame T kirk "
 
     println("my name is $tim")
-    println("jim is $jim")
+    println("jim is $jim")*/
+    val  lives = 3
+
+    if (lives < 1){
+        println("Game over ")
+    }else{
+        println("you're still slive!")
+    }
+
 }
