38849186e8bc82af0afae1ede7845c265963cf79
acer <wongsatorn1501003@gmail.com>
2019-10-15 13:45:54 +0700

    15

--- app/src/main/java/com/example/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial/MainActivity.kt
@@ -2,13 +2,12 @@ fun main(arg:Array<String>){
     println("Hello world")
     println("My first Kotlin program")
 
-
-    var tim: String
-    tim = "tim Buchalka"
+    val jim : String = "jame T kirk"
+    val tim: String = "tim Buchalka"
     println(tim)
 
-    var timweeklySalary : Int= 32
-    var monthly : Int = timweeklySalary * 4
+    val timweeklySalary : Int= 32
+    val monthly : Int = timweeklySalary * 4
     println("Tim's weekly salaly is${timweeklySalary}")
     println("monthly, that cones to $monthly ")
 
@@ -16,7 +15,7 @@ fun main(arg:Array<String>){
 
     val apples: Int = 6
     val oranges: Int = 5
-    var  frult : Int = apples - oranges
+    val  frult : Int = apples - oranges
     println("$apples apples - $oranges oranges leaves $frult place(s) of fruit")
 
     println("A quarter of the apples is ${apples / 4}")
@@ -26,5 +25,8 @@ fun main(arg:Array<String>){
     val  year: Double = week / 52.0
     println("130 weeks in year is : " + year)
 
+    //tim = "jame T kirk "
+
     println("my name is $tim")
+    println("jim is $jim")
 }
