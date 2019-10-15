5a2bb1c1cb2e1397c79f8d08b345bc950759f679
acer <wongsatorn1501003@gmail.com>
2019-10-15 13:20:35 +0700

    5

--- app/src/main/java/com/example/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial/MainActivity.kt
@@ -6,4 +6,9 @@ fun main(arg:Array<String>){
     var tim: String
     tim = "32"
     println(tim)
+
+    var timSalary : Int= 32
+    var monthly : Int = timSalary * 4
+    println(timSalary)
+    println(monthly)
 }
